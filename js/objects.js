47c0aa622d1dde0064ef0d1595f70cbfd8a08572
Glenn Y. Rolland <glenux@gmail.com>
2010-10-29 21:01:45 +0200

    Added Objet desc.

---
+++ js/objects.js
@@ -0,0 +1,28 @@
+
+
+function Map() {
+	function initialize() {
+	}
+}
+
+function Hole() {
+	var x,y;
+	var size;
+
+	function initialize() {
+	}
+}
+
+function Ball() {
+	var x,y;
+	var size;
+
+	function initialize() {
+	}
+}
+
+function Racket() {
+	function initialize() {
+	}
+}
+
