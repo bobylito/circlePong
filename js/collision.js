54029c829de5c08d8eed04055ddc794ca18387b0
Glenn Y. Rolland <glenux@gmail.com>
2010-10-30 00:44:24 +0200

    js/collision: encapsulated prototype extensions.

--- js/collision.js
+++ js/collision.js
@@ -1,18 +1,20 @@
 
+function initModuleCollision() {
 
-Map.prototype.collide = function( candidate ) {
+    Map.prototype.collide = function( candidate ) {
 
-}
+    }
 
-Hole.prototype.collide = function( candidate ) {
+    Hole.prototype.collide = function( candidate ) {
 
-}
+    }
 
-Ball.prototype.collide = function( candidate ) {
+    Ball.prototype.collide = function( candidate ) {
 
-}
+    }
 
-Map.prototype.collide = function( candidate ) {
+    Map.prototype.collide = function( candidate ) {
 
+    }
 }
 
