f8032a1cfa9cb847cbd47566e90a77f5ef005e88
Glenn Y. Rolland <glenux@gmail.com>
2010-11-12 22:02:02 +0100

    object.js: Added ball speed.

--- js/objects.js
+++ js/objects.js
@@ -1,6 +1,7 @@
 MAP_WIDTH = 300;
 MAP_HEIGHT = 300;
 BALL_SIZE = 20;
+BALL_SPEED = 20;
 HOLE_SIZE = 21;
 
 function World() {
@@ -8,19 +9,19 @@ function World() {
     this.holes = [];
     this.rackets = [];
     this.map = null;
-    this.context = 
+    this.context = "";
 
     this.addCollider = function( elem ) {
-	this.colliders[ this.colliders.length ] = elem;
+		this.colliders[ this.colliders.length ] = elem;
     }
     this.addBall = function( elem ){
-	this.balls[ this.balls.length ] = elem;
+		this.balls[ this.balls.length ] = elem;
     }
     this.addHole = function( elem ){
-	this.holes[ this.holes.length ] = elem;
+		this.holes[ this.holes.length ] = elem;
     }
     this.addRacket = function( elem ){
-	this.rackets[ this.rackets.length ] = elem;
+		this.rackets[ this.rackets.length ] = elem;
     }
 }
 
@@ -34,7 +35,7 @@ function Map( world, container ) {
     this.container.width = MAP_WIDTH;
     this.container.height = MAP_HEIGHT;
     world.map = this;
-    world.context =this.container.getContext('2d');
+    world.context = this.container.getContext('2d');
 }
 
 function Hole( world, x,y ) {
@@ -47,26 +48,34 @@ function Hole( world, x,y ) {
 
 function Ball( world, x, y ) {
     this.world = world;
-    // FIXME: initial position
-    this.x;
-    this.y;
-    // FIXME: initial vector
-    this.dx;
-    this.dy;
+    // initial position
+    this.x = MAP_WIDTH / 2;
+    this.y = MAP_HEIGHT / 2;
+    // initial movement
+	// FIXME: accelerate ball with time
+	angle = Math.random() * 2 * Math.PI;
+    this.dx = Math.cos( angle ) * BALL_SPEED;
+    this.dy = Math.sin( angle) * BALL_SPEED;
     this.size = BALL_SIZE;
+	console.log( "Ball.new( x = " + this.x + " ; y = " + this.y + " ; dx = " + this.dx + " ; dy = " + this.dy + " )");
     world.addBall( this );
 }
 
-function Racket(initPos) {
+function Racket( initPos ) {
     this.world = world;
-    this.pos=initPos;
+    this.pos = initPos;
     world.addRacket( this );
 }
 
 window.onload = function(){
-    //FIXME: load modules for drawing/etc.
-    //FIXME: add something to handle key strokes into objects
+    // load modules for drawing/etc.
+	initModuleDraw();
+	initModuleCollision();
+	initModuleEvent()
+
     var world = new World();
     var ball = new Ball( world );
     var map = new Map( world, "map");
+	var interval = setInterval( function() { world.update(); }, 40 );
 }
+
