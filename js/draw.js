10532f6bfce49765e9bd4a15b0f8ea104900a2ee
Glenn Y. Rolland <glenux@gmail.com>
2010-11-12 22:02:50 +0100

    draw.js: Added draw method to remaining objects.

--- js/draw.js
+++ js/draw.js
@@ -3,22 +3,27 @@
 function initModuleDraw() {
 
     function cleanContext( func ){
-	return function(){
-	    ctx.saveContext();
-	    func();
-	    ctx.restoreContext();
-	}
+		return function(){
+			ctx.saveContext();
+			func();
+			ctx.restoreContext();
+		}
     }
 
-    Hole.prototype.draw= cleanContext( function(){
+	World.prototype.draw = cleanContext( function() {
+
+	}); 
+
+    Hole.prototype.draw = cleanContext( function(){
 
 	});
 
-    Ball.prototype.draw=cleanContext( function(){
+    Ball.prototype.draw = cleanContext( function(){
 
 	});
 
-    Racket.prototype.draw=cleanContext( function(){
+    Racket.prototype.draw = cleanContext( function(){
 
 	});
 }
+
